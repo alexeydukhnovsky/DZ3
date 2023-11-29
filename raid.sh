9d614341bc221ab09e25e1c18f30c4e7bfec0e76
alexeydukhnovsky <61446579+alexeydukhnovsky@users.noreply.github.com>
2023-11-29 11:32:22 +0300

    Add files via upload

---
+++ raid.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+sudo -i
+mdadm --zero-superblock --force /dev/sd{b,c,d,e,f}
+mdadm --create --verbose /dev/md0 -l 5 -n 5 /dev/sd{b,c,d,e,f}
+mkdir /etc/mdadm
+echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
+mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf
